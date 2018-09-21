589da6e16f2f9d1952d4ed0ed4553bdf0b7a869c
Siyun He <siyun.he@yale.edu>
2018-09-21 13:02:38 -0400

    update

--- R/linear-models.r
+++ R/linear-models.r
@@ -10,29 +10,6 @@
 #' fit <- linear_model(Sepal.Length ~., iris)
 #' summary(fit)
 #' @export
-
-#load("/Users/siyunhe/Desktop/Year 2/BIS557/bis557/data/lm_patho.rda")
-
 linear_model <- function(formula, data) {
-  #browser()
   lm(formula, data)
 }
-#fit_test <- linear_model(y~.,data=lm_patho)
-#fit <- lm(lm_patho$y~.,lm_patho)
-#fit_test <- linear_model(Sepal.Length ~., iris)
-#fit <- lm(Sepal.Length ~., iris)
-
-
-#install.packages("testthat")
-
-#library(testthat)
-
-#library(devtools)
-#setwd("/Users/siyunhe/Desktop/Year 2/BIS557/bis557")
-#test( )
-
-
-
-
-
-
