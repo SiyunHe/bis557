ce62bb4dcf7e37b1ae2085bfd434573b3119bf01
Siyun He <siyun.he@yale.edu>
2018-09-21 12:50:21 -0400

    update

--- R/linear-models.r
+++ R/linear-models.r
@@ -15,18 +15,13 @@ load("/Users/siyunhe/Desktop/Year 2/BIS557/bis557/data/lm_patho.rda")
 
 linear_model <- function(formula, data) {
   #browser()
-  a<- all.vars(formula)
-  y <- data[,a[1]]
-  mm <- model.matrix(formula, data)
-  x<-mm
-  output <- list()
-  output$coefficients <- qr.coef(qr(x),y)
-  #output$call <- call("linear_model",formula)
-  class (output) = "lm"
-  return(output)
+  lm(formula, data)
 }
 #fit_test <- linear_model(y~.,data=lm_patho)
 #fit <- lm(lm_patho$y~.,lm_patho)
+#fit_test <- linear_model(Sepal.Length ~., iris)
+#fit <- lm(Sepal.Length ~., iris)
+
 
 #install.packages("testthat")
 
