02c37ce18e53b105b38fdf1cd06a593194fa808a
Siyun He <siyun.he@yale.edu>
2018-09-21 16:33:26 -0400

    hw1

--- R/linear-models.r
+++ R/linear-models.r
@@ -9,11 +9,14 @@
 #' @export 
 linear_model <- function(formula, data) {
   a<- all.vars(formula)
+  #create response varible y
   y <- data[,a[1]]
+  #create design matrix containing explanatory varibls and intercept
   mm <- model.matrix(formula, data)
   x<-mm
   output <- list()
   output$coefficients <- qr.coef(qr(x),y)
+  #create lm object
   class (output) = "lm"
   return(output)
 }
