73c66cf84be6009b4cbdd6f72cae8eeccfb85204
Siyun He <siyun.he@yale.edu>
2018-10-13 22:56:05 -0400

    hw2

--- data-raw/make-df-hw2.R
+++ data-raw/make-df-hw2.R
@@ -1,7 +1,6 @@
+setwd("~/Desktop/Year 2/BIS557/bis557/data-raw/")
 ridge_train <- read.csv("ridge_train.csv")
-dir.create("../data")
 save(ridge_train, file = "../data/ridge_train.rda")
 
 ridge_test <- read.csv("ridge_test.csv")
-dir.create("../data")
 save(ridge_test, file = "../data/ridge_test.rda")
